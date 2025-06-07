5d00916b92bbb2dae10eb7f25c8ed066584e16d8
Lionel <phamngoctuong1805@gmail.com>
2025-06-08 06:44:02 +0700

    Update 06.45AM 08.06

--- src/components/ui/EmblaCarouselCenter.tsx
+++ src/components/ui/EmblaCarouselCenter.tsx
@@ -29,26 +29,26 @@ const EmblaCarouselCenter = () => {
     }
   ];
   return (
-    <section className={`relative mx-auto max-w-[95%] md:max-w-[85%] mb-[110px]  2xl:max-w-[1580px]`}>
+    <section className={`relative mx-auto my-[50px] max-w-full md:max-w-[85%] 2xl:mb-[110px]  2xl:max-w-[1580px]`}>
       <Carousel
         opts={{
           align: "center",
           loop: true,
         }}
       >
-        <CarouselContent className="w-full h-[435px]">
+        <CarouselContent className="w-full items-center 2xl:h-[435px] -ml-0">
           {slides.map((event, index) => (
-            <CarouselItem key={index} index={index} className={`md:basis-1/3 h-full`}>
+            <CarouselItem key={index} index={index} className={`basis-3/4 sm:basis-1/3 h-full`}>
               <div className="relative w-full h-full flex items-center justify-center">
                 <div className="z-10 flex flex-wrap gap-[30px] items-center justify-center sliderstaff">
-                  <Image src={event.image} alt="event" width={630} height={435} />
+                  <Image  src={event.image} alt="event" width={630} height={435} />
                 </div>
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPreviousCenter variant="link" className="inline-flex items-center justify-center absolute  left-[-20px] top-1/2 -translate-y-1/2" />
-        <CarouselNextCenter variant="link" className="inline-flex items-center justify-centerabsolute   right-[15px] top-1/2 -translate-y-1/2" />
+        <CarouselPreviousCenter variant="link" className="hidden md:inline-flex items-center justify-center absolute  left-[-20px] top-1/2 -translate-y-1/2" />
+        <CarouselNextCenter variant="link" className="hidden md:inline-flex items-center justify-center absolute   right-[-20px] top-1/2 -translate-y-1/2" />
       </Carousel>
     </section>
   )
