eca803b4288cc4def5175b3056c5b1a118495492
Lionel <phamngoctuong1805@gmail.com>
2025-06-14 14:44:34 +0700

    Update 02.40AM 14.06

--- src/app/(public)/[locale]/ecosystem/page.tsx
+++ src/app/(public)/[locale]/ecosystem/page.tsx
@@ -1,9 +1,15 @@
 import MasonryGrid from "@/components/ecosystem/MasonryGrid"
-export default function Ecosystem() {
+export default async function Ecosystem() {
+  const res = await fetch('https://admin.pigroup.tqdesign.vn/api/pages/ecosystem', {
+    cache: 'no-store',
+  });
+  const {data} = await res.json();
+  const { custom_fields } = data;
+  const {field_ecosystem} = custom_fields;
   return (
     <>
       <div>
-        <MasonryGrid />
+        <MasonryGrid custom_fields={field_ecosystem} />
       </div>
     </>
   )
--- src/components/ecosystem/MasonryGrid.tsx
+++ src/components/ecosystem/MasonryGrid.tsx
@@ -108,8 +108,20 @@ interface GridConfig {
   fixedElements: ElementRef[];
   expandableElements: ElementRef[];
 }
-const MasonryGrid: React.FC = () => {
-   useScrollReveal(); // dùng mặc định `.boxanimation`
+const MasonryGrid = ({ custom_fields }: any) => {
+  function convertJsonStringToArrayOrObject(jsonString: string): any | null {
+    try {
+      const parsedData = JSON.parse(jsonString);
+      return parsedData;
+    } catch (error) {
+      console.error("Error parsing JSON string:", error);
+      return null; // Return null or throw the error, depending on your error handling preference
+    }
+  }
+  const customfields = convertJsonStringToArrayOrObject(custom_fields);
+  // const {field_1, field_2, field_3, field_4} = custom_fields || {};
+  console.log("customfields", customfields);
+  useScrollReveal(); // dùng mặc định `.boxanimation`
   const rows = [];
   // Refs cho từng grid container
   const grid1Ref = useRef<HTMLDivElement>(null);
@@ -204,8 +216,9 @@ const MasonryGrid: React.FC = () => {
       };
     });
   }, []); // Chạy một lần khi component mount
-  for (let i = 0; i < items.length; i += 4) {
-    const rowItems = items.slice(i, i + 4);
+  let j = 0;
+  for (let i = 0; i < customfields.length; i += 4) {
+    const rowItems = customfields.slice(i, i + 4);
     // Nếu là dòng 2 (phần tử 5–8): dùng grid-cols-[repeat(3,1fr)_45%]
     const isSpecialRow = i === 4;
     rows.push(
@@ -214,32 +227,41 @@ const MasonryGrid: React.FC = () => {
         ref={(i === 0) ? grid1Ref : (i === 4) ? grid2Ref : (i === 8) ? grid3Ref : null}
         className={`grid transition-all duration-[500ms] ease-in-out gap-x-[2px] m-0 xl:h-[calc((100vh-138px)/3)] grid-cols-3 ${isSpecialRow ? "xl:grid-cols-[repeat(3,1fr)_42%]" : "xl:grid-cols-[42%_repeat(3,1fr)]"}`}
       >
-        {rowItems.map((item, index) => (
-          <div key={item.id} ref={(i === 0 && index == 0) ? m1Ref : (i === 0 && index == 1) ? m2Ref : (i === 0 && index == 2) ? m3Ref : (i === 0 && index == 3) ? m4Ref : (i === 4 && index == 0) ? m5Ref : (i === 4 && index == 1) ? m6Ref : (i === 4 && index == 2) ? m7Ref : (i === 4 && index == 3) ? m8Ref : (i === 8 && index == 0) ? m9Ref : (i === 8 && index == 1) ? m10Ref : (i === 8 && index == 2) ? m11Ref : (i === 8 && index == 3) ? m12Ref : null} className={`group eco-item transition-all duration-[500ms] ease-in-out relative pt-[300px] xl:pt-[33.33%] overflow-hidden ecosystem-masonry-${item.id} ${index === 0 ? 'col-span-3 xl:col-span-1' : 'col-span-1'}`}>
-            <Image src={item.image} alt="masonry" fill className="object-cover justify-end" />
-            {item.title && !item.link && (
-              <h3 className="z-10 left-[25px] bottom-[8px] text-white absolute text-[18px] 2xl:text-[26px] font-semibold uppercase opacity-0 group-hover:opacity-100 duration-500">
-                {item.title}
-              </h3>
-            )}
-            {item.title && item.link && (
-              <div className={`absolute inset-0 bg-white flex items-center  ${index === 3 ? 'justify-end xl:pl-[100px]' : 'justify-center xl:pr-[100px]'} duration-300`}>
-                <div className="flex flex-col justify-center">
-                  <h3 className="text-[19px] 2xl:text-[25px] font-semibold text-blue-1 uppercase">
-                    {item.title}
-                  </h3>
-                  <p className="text-gray-6 text-justify mt-[10px] mb-[12px] text-[13px] 2xl:text-[17px]">{item.description}</p>
-                  <Link
-                    href={item.link}
-                    className="flex items-center justify-center text-yellow-1 font-semibold w-[116px] h-[28px] text-[12px] 2xl:text-[16px] 2xl:w-[138px] 2xl:h-[35px] border border-yellow-1 hover:text-amber-50 hvr-bounce-to-right duration-300"
-                  >
-                    XEM THÊM
-                  </Link>
+        {rowItems.map((item: any, index: number) => {
+          j++;
+          console.log(item[4].value);
+          return (
+            <div key={item[0].field_item_id + index} ref={(i === 0 && index == 0) ? m1Ref : (i === 0 && index == 1) ? m2Ref : (i === 0 && index == 2) ? m3Ref : (i === 0 && index == 3) ? m4Ref : (i === 4 && index == 0) ? m5Ref : (i === 4 && index == 1) ? m6Ref : (i === 4 && index == 2) ? m7Ref : (i === 4 && index == 3) ? m8Ref : (i === 8 && index == 0) ? m9Ref : (i === 8 && index == 1) ? m10Ref : (i === 8 && index == 2) ? m11Ref : (i === 8 && index == 3) ? m12Ref : null} className={`group eco-item transition-all duration-[500ms] ease-in-out relative pt-[300px] xl:pt-[33.33%] overflow-hidden ecosystem-masonry-${j} ${index === 0 ? 'col-span-3 xl:col-span-1' : 'col-span-1'}`}>
+              {item[4].value !=="" ? (
+                <Image src={`https://admin.pigroup.tqdesign.vn/storage/${item[4].value}`} alt="masonry" fill className="object-cover justify-end" />
+              ) : (
+                null
+              )}
+              {item[0].value && item[4].value !== "" && (
+                <h3 className="z-10 left-[25px] bottom-[8px] text-white absolute text-[18px] 2xl:text-[26px] font-semibold uppercase opacity-0 group-hover:opacity-100 duration-500">
+                  {item[0].value}
+                </h3>
+              )}
+              {item[0].value && item[3].value !== "" && (
+                <div className={`absolute inset-0 bg-white flex items-center  ${index === 3 ? 'justify-end xl:pl-[100px]' : 'justify-center xl:pr-[100px]'} duration-300`}>
+                  <div className="flex flex-col justify-center">
+                    <h3 className="text-[19px] 2xl:text-[25px] font-semibold text-blue-1 uppercase">
+                      {item[0].value}
+                    </h3>
+                    <p className="text-gray-6 text-justify mt-[10px] mb-[12px] text-[13px] 2xl:text-[17px]">{item[1].value}</p>
+                    <Link
+                      href={item[3].value}
+                      className="flex items-center justify-center text-yellow-1 font-semibold w-[116px] h-[28px] text-[12px] 2xl:text-[16px] 2xl:w-[138px] 2xl:h-[35px] border border-yellow-1 hover:text-amber-50 hvr-bounce-to-right duration-300"
+                    >
+                      {item[2].value}
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            )}
-          </div>
-        ))}
+              )}
+            </div>
+          )
+        }
+        )}
       </div>
     );
   }
