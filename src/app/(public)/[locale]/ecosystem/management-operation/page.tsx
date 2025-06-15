721f909202cacd031cfde8fc3cf0902f992a16c9
Lionel <phamngoctuong1805@gmail.com>
2025-06-16 01:20:51 +0700

    Update 01:20:AM 1606

--- src/app/(public)/[locale]/ecosystem/management-operation/page.tsx
+++ src/app/(public)/[locale]/ecosystem/management-operation/page.tsx
@@ -1,12 +1,14 @@
-'use client';
 import Detail3 from "@/components/ecosystem/Detail3"
-import Head from "next/head"
-import { useEffect } from "react";
-export default function EcosystemDetail() {
+export default async function EcosystemDetail() {
+  const res = await fetch('https://admin.pigroup.tqdesign.vn/api/pages/management-operation', {
+    cache: 'no-store',
+  });
+  const {data} = await res.json();
+  const { custom_fields } = data;
   return (
     <>
       <div>
-        <Detail3 />
+        <Detail3 custom_fields={custom_fields}/>
       </div>
     </>
   )
