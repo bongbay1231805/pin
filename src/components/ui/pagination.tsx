9b0a7e71a1a46fa580357dd47ac887fe906e3a76
Lionel <phamngoctuong1805@gmail.com>
2025-06-16 05:10:56 +0700

    Update 05:10:AM 1606

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -59,7 +59,7 @@ const PaginationPrevious = ({
   <PaginationLink
     aria-label="Go to previous page"
     size="default"
-    className={cn("gap-1 pl-2.5", className)}
+    className={cn("gap-1 pl-2.5 hidden!", className)}
     {...props}
   >
     <ChevronLeft className="h-4 w-4" />
@@ -74,7 +74,7 @@ const PaginationNext = ({
   <PaginationLink
     aria-label="Go to next page"
     size="default"
-    className={cn("gap-1 pr-2.5", className)}
+    className={cn("gap-1 pr-2.5 hidden!", className)}
     {...props}
   >
     <span>Next</span>
