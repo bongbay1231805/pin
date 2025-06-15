5f4fac8e9b9429bbd0cba19ee17409aba85548ce
Lionel <phamngoctuong1805@gmail.com>
2025-06-15 09:54:13 +0700

    Update 10.00AM 15.06

--- src/app/(public)/[locale]/SubNavbar.tsx
+++ src/app/(public)/[locale]/SubNavbar.tsx
@@ -35,7 +35,7 @@ export default function SubNavbar(props: PropSub) {
     requestAnimationFrame(animation);
   };
   let navItems: { name: string, href: string, hrefb?: React.RefObject<HTMLDivElement | null> | undefined }[] = [];
-  const ecosystem = ['ecosystem', 'investment-development', 'real-estate-services', 'management-operation'];
+  const ecosystem = ['investment-development', 'real-estate-services', 'management-operation'];
   const news = ['news', 'market-news', 'pi-group-news', 'bidding-news'];
   const humanresource = ['human-resource'];
   const about = ['about'];
@@ -92,6 +92,22 @@ export default function SubNavbar(props: PropSub) {
       {
         name: "Tin đấu thầu",
         href: "/news/bidding-news"
+      }
+    ];
+  }
+  if (ecosystem.includes(nameCurent.split("/").pop() || "")) {
+    navItems = [
+      {
+        name: "Đầu tư và phát triển dự án",
+        href: "/ecosystem/investment-development"
+      },
+      {
+        name: "Dịch vụ bất động sản",
+        href: "/ecosystem/real-estate-services"
+      },
+      {
+        name: "Quản lý và vận hành",
+        href: "/ecosystem/management-operation"
       }
     ];
   }
@@ -161,16 +177,18 @@ export default function SubNavbar(props: PropSub) {
   return (
     (Array.isArray(navItems) && navItems.length) ? (
       <div className={`w-full hidden xl:block`} >
-        <div className="mx-auto max-w-[91.4%] bg-gray-3 border-white-1 border-b-[1px]">
+        <div className="mx-auto bg-gray-3 border-white-1 border-b-[1px]">
           <ul className="flex flex-wrap space-x-2 ef:space-x-6 justify-center gap-[38px] ef:gap-[38px] py-[8px] text-gray-5">
             {navItems.map((item) => (
               item.hrefb ? (
-                <button onClick={() => scrollTo(item.hrefb!)} key={item.name} className={`text-[12px] 2xl:text-[16px] cursor-pointer font-regular hover:text-yellow-1 focus:text-yellow-1 focus-visible:text-yellow-1 active:text-yellow-1 ${isActive(item.href) ? 'text-yellow-1' : ''}`}>
-                  {item.name}
-                </button>
+                <li key={item.name}>
+                  <button onClick={() => scrollTo(item.hrefb!)} key={item.name} className={`text-[12px] 2xl:text-[16px] cursor-pointer font-regular hover:text-yellow-1 focus:text-yellow-1 focus-visible:text-yellow-1 active:text-yellow-1 ${isActive(item.href) ? 'text-yellow-1' : ''}`}>
+                    {item.name}
+                  </button>
+                </li>
               ) : (
                 <li key={item.name}>
-                  <Link href={item.href} className={`text-[13px] 2xl:text-[17px]  font-regular hover:text-yellow-1 focus:text-yellow-1 focus-visible:text-yellow-1 ${isActive(item.href) ? 'text-yellow-1' : ''}`}>  
+                  <Link href={item.href} className={`text-[12px] 2xl:text-[16px] cursor-pointer font-regular hover:text-yellow-1 focus:text-yellow-1 focus-visible:text-yellow-1 active:text-yellow-1 ${isActive(item.href) ? 'text-yellow-1' : ''}`}>
                     {item.name}
                   </Link>
                 </li>
