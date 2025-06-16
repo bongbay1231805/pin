4a40b679055161af2d7f7276a468601434c599cd
Lionel <phamngoctuong1805@gmail.com>
2025-06-16 07:41:51 +0700

    Update 07:40:AM 1606

---
+++ src/app/(public)/[locale]/posts/[slug]/page.tsx
@@ -0,0 +1,45 @@
+import { ContentBidding } from '@/components/news/ContentBidding';
+import { Hero } from '@/components/news/Hero';
+import Related from '@/components/news/Related';
+import { RegistrationForm } from '@/components/news/RegistrationForm';
+type Params = {
+  params: {
+    slug: string;
+    locale: string;
+  };
+};
+async function getPostBySlug(slug: string) {
+  const res = await fetch(`https://admin.pigroup.tqdesign.vn/api/posts/${slug}`, {
+    cache: 'no-store',
+  });
+  if (!res.ok) {
+    // Handle 404
+    return null;
+  }
+  const json = await res.json();
+  return json.data;
+}
+async function DetailPost({ params }: Params) {
+  const {slug} = await params;
+  const post = await getPostBySlug(slug);
+  if (!post) {
+    return <div className="text-center mt-20">Không tìm thấy bài viết</div>;
+  }
+  return (
+    <div>
+      <Hero post={post}/>
+      <div className="container m-auto max-w-[95%] md:max-w-[76%] 2xl:max-w-[1312px]">
+        <ContentBidding />
+      </div>
+      <div className="container m-auto max-w-[85%] rounded-[10px] bg-[#EAF3FF]/50 mt-[80px] px-[90px] pt-[70px] pb-[70px]">
+        <h2 className='text-yellow-1 uppercase text-center text-size-30 md:text-size-35 lg:text-[38px] 2xl:text-[45px] font-bold mb-[35px]'>Đăng ký dự thầu</h2>
+        <RegistrationForm />
+      </div>
+      <div className="container m-auto max-w-[85%] px-[10px]">
+        <h2 className='text-yellow-1 uppercase text-center text-[35px] 2xl:text-[45px] font-bold mb-[45px] mt-[90px]'>Tin liên quan</h2>
+        <Related />
+      </div>
+    </div>
+  );
+}
+export default DetailPost;
---
+++ src/components/posts/Post.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { useScrollReveal } from "@/hooks/useScrollReveal";
+import Image from "next/image"
+export function Post() {
+  useScrollReveal();
+  return (
+    <div>
+      aa
+    </div>
+  )
+}
