c9a92399e15a055d5fbe1a291d740c0c16b68548
Lionel <phamngoctuong1805@gmail.com>
2025-06-17 18:39:30 +0700

    Update 06:40:PM 1806

--- src/components/about/Philosophy.tsx
+++ src/components/about/Philosophy.tsx
@@ -3,24 +3,22 @@ import Image from "next/image"
 import { useScrollRefs } from '@/context/ScrollRefsContext'
 export function Philosophy({ custom_fields }: any) {
   const { fourRef, fiveRef } = useScrollRefs();
-  const { field_13_about, field_14_about, field_15_about, field_16_about, field_17_about, field_18_about, field_19_about, field_20_about, field_21_about, field_22_about, field_22_about_174915960724, field_23_about } = custom_fields;
+  const {field_13_about,field_14_about,field_15_about,field_16_about,field_17_about,field_18_about,field_19_about,field_20_about,field_21_about,field_22_about,field_22_about_174915960724,field_23_about} = custom_fields;
   return (
-    <section ref={fourRef} className="py-[30px] sm:py-[50px]">
-      <div className="px-[30px] w-full sm:max-w-[85%] mx-auto">
-        <div className="bg-blue-3 rounded-[10px] bg-[url('/fabout/target.png')] bg-contain bg-no-repeat bg-right">
-          <div className="grid items-center grid-cols-1 md:grid-cols-[70%_30%] mb-8 uppercase px-[20px] li:px-[30px] text-[16px] li:text-[20px] 2xl:text-[35px] py-[2px] text-white">
-            <div className="pb-[22px] font-bold text-start leading-[30px] 2xl:leading-[42px] pt-[10px] sm:pl-[106px]">
-              <p>{field_19_about}<br /> {field_20_about}</p>
-            </div>
-            <div className="text-end py-[22px] leading-[30px]  sm:leading-[52px] 2xl:leading-[62px] font-[500]">
-              <h3>{field_13_about} <span className="text-yellow-1">{field_14_about}</span></h3>
-              <h3>{field_15_about} <span className="text-yellow-1">{field_16_about}</span></h3>
-              <h3>{field_17_about} <span className="text-yellow-1">{field_18_about}</span></h3>
-            </div>
+    <section ref={fourRef} className="py-16">
+      <div className="max-w-[85%] mx-auto bg-blue-3 rounded-[10px] bg-[url('/fabout/target.png')] bg-contain bg-no-repeat bg-right">
+        <div className="grid items-center grid-cols-1 md:grid-cols-[30%_70%] mb-8 uppercase text-[16px] li:text-[20px] 2xl:text-[35px] py-[2px] text-white">
+          <div className="py-[22px] font-bold leading-[30px] text-[23px] 2xl:text-[35px] 2xl:leading-[42px] pl-[30px] sm:pl-[106px]">
+            <p>{field_19_about}<br />{field_20_about}</p>
+          </div>
+          <div className="text-end py-[22px] pr-[30px] sm:pr-[106px] leading-[30px] sm:leading-[52px] 2xl:leading-[62px] font-[500]">
+            <h3>{field_13_about} <span className="text-yellow-1">{field_14_about}</span></h3>
+            <h3>{field_15_about} <span className="text-yellow-1">{field_16_about}</span></h3>
+            <h3>{field_17_about} <span className="text-yellow-1">{field_18_about}</span></h3>
           </div>
         </div>
       </div>
-      <div ref={fiveRef} className="px-[30px] w-full sm:max-w-[85%] m-auto">
+      <div ref={fiveRef} className="max-w-[85%] m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="relative vision-item vision-item h-[320px] 2xl:h-[380px] bg-gray-800 group rounded-[10px] overflow-hidden">
             <div className="absolute inset-0 bg-black/50 flex items-center justify-center">
