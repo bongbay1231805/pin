5ade1815314c24745833dfa9de84dee1d8cdae4d
thducuit <thducuit@gmail.com>
2025-07-02 18:41:42 +0700

    fix menu

--- src/components/home/Partners.tsx
+++ src/components/home/Partners.tsx
@@ -68,7 +68,7 @@ export function Partners({
                       alt="Partner logo"
                       width={partner.width}
                       height={partner.height}
-                      className="grayscale-[100%] opacity-75 hover:grayscale-0 hover:opacity-100"
+                      className="sm:grayscale-[100%] opacity-75 hover:grayscale-0 hover:opacity-100"
                       style={{objectFit: 'contain'}}
                     />
                   </div>
