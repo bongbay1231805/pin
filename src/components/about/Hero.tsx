38a4c0a7755d2d46819f11351afc74835924eaea
Bao Nguyen <baobao271089@gmail.com>
2025-07-04 16:57:12 +0700

    change banner home,about

--- src/components/about/Hero.tsx
+++ src/components/about/Hero.tsx
@@ -5,7 +5,7 @@ export function Hero({image}: {image?: string}) {
   useScrollReveal(); // dùng mặc định `.boxanimation`
   const imageSrc = image
     ? `https://admin.pigroup.tqdesign.vn/storage/${image}`
-    : '/fabout/cityscape.jpg';
+    : '/fabout/TT.png';
 
   return (
     <div className="relative h-[500px] lg:h-[100vh] w-full">
