6f0766350af7d6dce98ce0e60cac09bfb09b8256
Duong Hoang Tuan <tuan.duong@fullertonhealth.com>
2025-07-07 22:53:03 +0700

    fix build

--- src/app/NavLink.tsx
+++ src/app/NavLink.tsx
@@ -1,17 +1,18 @@
 'use client';
-import {useSelectedLayoutSegment} from 'next/navigation';
+// import {useSelectedLayoutSegment} from 'next/navigation';
 import {ComponentProps} from 'react';
-import {Link} from '@/i18n/navigation.public';
+import Link from 'next/link';
 export default function NavLink({href, ...rest}: ComponentProps<typeof Link>) {
-  const selectedLayoutSegment = useSelectedLayoutSegment();
-  const pathname = selectedLayoutSegment ? `/${selectedLayoutSegment}` : '/';
-  const isActive = pathname === href;
+  // const selectedLayoutSegment = useSelectedLayoutSegment();
+  // const pathname = selectedLayoutSegment ? `/${selectedLayoutSegment}` : '/';
+  // const isActive = pathname === href;
   return (
-    <Link
-      aria-current={isActive ? 'page' : undefined}
-      className={isActive ? 'font-semibold' : undefined}
-      href={href}
-      {...rest}
-    />
+    // <Link
+    //   aria-current={isActive ? 'page' : undefined}
+    //   className={isActive ? 'font-semibold' : undefined}
+    //   href={href}
+    //   {...rest}
+    // />
+    <></>
   );
 }
