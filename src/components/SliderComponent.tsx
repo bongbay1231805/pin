f2c6fc2ec0e734f5ede6428910bf60ad7987d608
thducuit <thducuit@gmail.com>
2025-07-18 07:07:09 +0700

    fix

--- src/components/SliderComponent.tsx
+++ src/components/SliderComponent.tsx
@@ -88,21 +88,8 @@ export default function SliderComponent({ slides }: Props) {
           className="absolute left-0 sm:left-0 top-1/2 -translate-y-1/2 p-2 rounded-full z-30   hover:bg-opacity-75 transition-colors duration-200 focus:outline-none text-yellow-1 rounded-full"
           aria-label="Previous Slide"
         >
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="w-6 h-6 text-gray-800 md:w-8 md:h-8"
-          >
-            <path d="m15 18-6-6 6-6" />
-          </svg> */}
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left" aria-hidden="true"><path d="m15 18-6-6 6-6"></path></svg>
+          
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-left" aria-hidden="true"><path d="m15 18-6-6 6-6"></path></svg>
         </button>
 
         <div className="relative w-full h-full perspective-1200">
@@ -137,21 +124,8 @@ export default function SliderComponent({ slides }: Props) {
           className="absolute right-0 sm:right-0 top-1/2 -translate-y-1/2 p-2 rounded-full z-30   hover:bg-opacity-75 transition-colors duration-200 focus:outline-none text-yellow-1 rounded-full"
           aria-label="Next Slide"
         >
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="w-6 h-6 text-gray-800 md:w-8 md:h-8"
-          >
-            <path d="m9 18 6-6-6-6" />
-          </svg> */}
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right" aria-hidden="true"><path d="m9 18 6-6-6-6"></path></svg>
+          
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-right" aria-hidden="true"><path d="m9 18 6-6-6-6"></path></svg>
         </button>
       </div>
 
